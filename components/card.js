c47bde125850fded4b664aefec46a30b456f6437
Carl 'Subzidion' Hiltbrunner <git@carl.hiltbrunner.email>
2023-11-04 19:00:04 -0700

    Tighten Card margin on mobile.

--- components/card.js
+++ components/card.js
@@ -1,6 +1,6 @@
 export default function Card(props) {
   return (
-    <div className="card border-solid border-black border-8 p-2 m-10">
+    <div className="card border-solid border-black border-8 p-2 m-5 lg:m-10">
       {props.children}
     </div>
   )

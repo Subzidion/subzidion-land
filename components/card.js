2cbef459babf3a3332caf257bb87cf409b9cacdd
Carl 'Subzidion' Hiltbrunner <git@carl.hiltbrunner.email>
2023-11-05 01:09:24 -0700

    CSS cleanup.

--- components/card.js
+++ components/card.js
@@ -1,6 +1,6 @@
 export default function Card(props) {
   return (
-    <div className="card border-solid border-black border-8 p-2 m-5 lg:m-10">
+    <div className="bg-seasalt border-solid border-black border-8 p-2 m-5 lg:m-10">
       {props.children}
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./components/*.{js,ts,jsx,tsx,mdx}",
@@ -6,6 +9,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      black: colors.black,
+      seasalt: "#FAFAFA",
+      jet: "#353535",
+      robinEggBlue: "#59C3C3",
+      myrtleGreen: "297373",
+      cambridgeBlue: "#84A98C",
+      roseQuartz: "#AD9BAA",
+      eggplant: "635160",
+    },
   },
   plugins: [],
 }
